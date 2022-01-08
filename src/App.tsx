a58f9b557f76435445a25246754c2cdfe3bc6d98
Nesquiko <lukascastven@gmail.com>
2022-01-08 13:24:03 +0100

    Add all forms to main App.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { DAppProvider, ChainId } from "@usedapp/core";
 import { Container } from "@material-ui/core";
 import { Header } from "./components/Header";
 import { CreateVotingSession } from "./components/CreateVotingSession";
+import { Vote } from "./components/Vote";
+import { AddChoice } from "./components/AddChoice";
 
 function App() {
     return (
@@ -16,9 +18,22 @@ function App() {
             }}
         >
             <Header />
-            <Container maxWidth="lg">
-                <CreateVotingSession />
-            </Container>
+            <div>
+                <Container maxWidth="lg">
+                    <CreateVotingSession />
+                </Container>
+            </div>
+            <div style={{ paddingTop: "200px" }}>
+                <Container maxWidth="lg">
+                    <AddChoice />
+                </Container>
+            </div>
+            <div style={{ paddingTop: "200px", paddingBottom: "50px" }}>
+                <Container maxWidth="lg">
+                    <Vote />
+                </Container>
+            </div>
+            <div style={{ paddingTop: "200px" }} />
         </DAppProvider>
     );
 }
