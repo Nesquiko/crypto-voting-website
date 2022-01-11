acbd38a4c396c3ebb3a31fcc8332f80b14ca0355
Nesquiko <lukascastven@gmail.com>
2022-01-11 12:53:16 +0100

    Handle changing of inputs.

--- src/components/AddChoiceForm.tsx
+++ src/components/AddChoiceForm.tsx
@@ -1,27 +1,58 @@
 import { Button, Input } from "@material-ui/core";
-import React from "react";
+import { useState } from "react";
+import { useAddChoice } from "./hooks/useAddChoice";
 
 export const AddChoiceForm = () => {
+    const [symbol, setSymbol] = useState<string>("");
+    const [choice, setChoice] = useState<string>("");
+
+    const handleSymbolChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSymbol(event.target.value);
+    };
+
+    const handleChoiceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChoice(event.target.value);
+    };
+
+    const addChoiceSetup = useAddChoice();
+    const { addChoice, addChoiceState } = addChoiceSetup(choice, symbol);
+    const handleAddChoice = () => {
+        if (symbol === "") {
+            alert(
+                "Please enter a symbol of voting session when adding a new choice."
+            );
+        }
+        if (choice === "") {
+            alert("Please enter a new choice.");
+        }
+
+        return addChoice();
+    };
+
     return (
         <>
             <div
                 style={{ float: "left", marginLeft: "50px", marginTop: "50px" }}
             >
                 <h3>Symbol</h3>
-                <Input />
+                <Input onChange={handleSymbolChange} />
             </div>
 
             <div
                 style={{ float: "left", marginLeft: "50px", marginTop: "50px" }}
             >
                 <h3>Choice</h3>
-                <Input />
+                <Input onChange={handleChoiceChange} />
             </div>
 
             <div
                 style={{ float: "left", marginLeft: "50px", marginTop: "50px" }}
             >
-                <Button color="primary" variant="contained">
+                <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={handleAddChoice}
+                >
                     Add
                 </Button>
             </div>
